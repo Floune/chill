a1ecf16dd15d0a9feeb688a2c8c2eb44efc6462f
RobinFloune <rfournier@simplonprod.co>
2020-09-04 18:18:34 +0200

    batman

---
+++ connexion.js
@@ -0,0 +1,13 @@
+'user strict';
+
+const Mysql = require('sync-mysql') 
+
+//local mysql db connection
+var connection = new Mysql({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const connexion = require('./connexion')
+const bodyParser = require('body-parser');
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+	res.send('cadavre exquis')
+})
+
+app.get('/shuffle', async (req, res) => {
+	const part1 = fetchParts(1)
+	const part2 = fetchParts(2)
+	const part3 = fetchParts(3)
+	res.json({
+		part1: part1,
+		part2: part2,
+		part3: part3,
+	})
+})
+
+app.post('/newdrawing', (req, res) => {
+	saveDrawing(req);
+})
+
+
+function saveDrawing(req) {
+	connexion.query("INSERT INTO images (image, part ) VALUES ( ?, ?)", [req.body.img, req.body.part], function(error, results, fields) {
+		if (error) throw error;
+		console.log(results);
+	})
+}
+
+function fetchParts(part) {
+	const response = connexion.query('SELECT image, part FROM images WHERE part = ? ORDER BY RAND() LIMIT 1', [part]);
+	return response;
+}
+
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
